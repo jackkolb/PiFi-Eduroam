333e959317d60d25f731d3e8d746f184459b5ff5
Jack Kolb <jackhunterkolb@gmail.com>
2018-10-06 19:52:03 -0700

    First (and hopefully last) commit!

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PiFi-Eduroam
-Shell script to connect your Raspberry Pi to University WiFi networks (ex: eduroam)
+Shell script to connect your Raspberry Pi to Eduroam!
+Generates and appends the correct network settings to the wpa_supplicant.conf file
+
+I made this after spending far too long searching and seeing this question come up
+ on numerous online threads... hopefully this helps you out :)
+
+Example output:
+'''
+[~] ./pifi.sh
+-- PiFi-Eduroam --
+Connects your Raspberry Pi to eduroam!
+
+Eduroam Username (id@domain.edu): jackkolb@ucr.edu
+Eduroam Password:
+
+Generating network entry...
+Complete! Reboot or reset wpa_supplicant
+'''
+
+
---
+++ pifi.sh
@@ -0,0 +1,34 @@
+#/usr/bin/sh
+
+# Created by Jack Kolb in 2018
+
+# PiFi-Eduroam: adds the network sequence for eduroam
+# to a raspberry pi's wpa_supplicant.conf file
+
+WPA_SUPPLICANT_FILE="/etc/wpa_supplicant/wpa_supplicant.conf"
+
+echo '-- PiFi-Eduroam --'
+echo 'Connects your Raspberry Pi to eduroam!'
+echo ''
+
+read -p 'Eduroam Username (id@domain.edu): ' USERNAME
+read -sp 'Eduroam Password: ' PASSWORD
+
+echo ''
+echo ''
+echo 'Generating network entry...'
+
+echo ''                       >> $WPA_SUPPLICANT_FILE
+echo 'networks={'             >> $WPA_SUPPLICANT_FILE
+echo '	ssid="eduroam"'       >> $WPA_SUPPLICANT_FILE
+echo '  scan_ssid=1'          >> $WPA_SUPPLICANT_FILE
+echo '	key_mgmt=WPA_EAP'     >> $WPA_SUPPLICANT_FILE
+echo '	group=CCMP TKIP'      >> $WPA_SUPPLICANT_FILE
+echo '	eap=PEAP'             >> $WPA_SUPPLICANT_FILE
+echo '	identity='$USERNAME   >> $WPA_SUPPLICANT_FILE
+echo '	password='$PASSWORD   >> $WPA_SUPPLICANT_FILE
+echo '	phase1="peapver=0"'   >> $WPA_SUPPLICANT_FILE
+echo '	phase2="WSCHAPv2"'    >> $WPA_SUPPLICANT_FILE
+echo '}'                      >> $WPA_SUPPLICANT_FILE
+
+echo 'Complete! Reboot or reset wpa_supplicant'
